e444fb5b8ef1e752c22b0b79550c38ddbf9fbba6
Hung Viet Nguyen <henry@gotitapp.co>
2020-12-01 09:15:01 +0700

    chore: bump 1.0.8

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ const util = require('util');
 const exec = util.promisify(require('child_process').exec);
 
 // TODO: Automate current version
-const CURRENT_VERSION = '1.0.7';
+const CURRENT_VERSION = '1.0.8';
 
 function getCurrentVersion() {
   return CURRENT_VERSION;
